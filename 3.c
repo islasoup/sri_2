a45a030ed309d15fb93521e7aaac679bba39bd93
Srinidhi B <141602477+islasoup@users.noreply.github.com>
2024-06-11 09:49:32 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,14 @@
+//ascii 1
+#include <stdio.h>
+
+int main() {
+  char ch;
+
+  // Prompt the user for a character
+  scanf("%c", &ch);
+
+  // Print the ASCII value of the character
+  printf("%d\n", ch, (int)ch);
+
+  return 0;
+}
